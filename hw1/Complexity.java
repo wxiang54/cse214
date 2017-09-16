98542079c703379481ea60d5f0ead895a604a7b8
will <willxiang1999@gmail.com>
2017-09-16 11:33:26 -0400

    hw1 init

---
+++ hw1/Complexity.java
@@ -0,0 +1,83 @@
+public class Complexity {
+
+    /* 1a (Complexity Analysis - Part a)
+     * pairSum() takes an array of int values and finds if there
+       is a pair of numbers with a given sum.
+       * PRECOND: none
+       * POSTCOND: returns true if sum found, false otherwise
+    */
+    public static boolean pairSum(int[] arr, int sum) {
+	for (int i = 0; i < arr.length; i++) {
+	    for (int j = i+1; j < arr.length; j++) {
+		if (arr[i] + arr[j] == sum) {
+		    System.out.println("Found pair: " + 
+				       arr[i] + " + " + arr[j] + 
+				       " = " + sum);
+		    return true;
+		}
+	    }
+	}
+	return false;
+    }
+
+
+    /* 1b (Complexity Analysis - Part b)
+     * tripletSum() takes an array of int values and finds if there
+       is a triplet of numbers with a given sum.
+       * PRECOND: none
+       * POSTCOND: returns true if sum found, false otherwise
+    */
+    public static boolean tripletSum(int[] arr, int sum) {
+	for (int i = 0; i < arr.length; i++) {
+	    for (int j = i+1; j < arr.length; j++) {
+		for (int k = i+2; k < arr.length; k++) {
+		    if (arr[i] + arr[j] + arr[k] == sum) {
+			System.out.println("Found triplet: " + arr[i] + 
+					   " + " + arr[j] + " + " +
+					   arr[k] + " = " + sum);
+			return true;
+		    }
+		}
+	    }
+	}
+	return false;
+    }
+    
+
+
+    // Helper method to print arrays
+    public static String printArray(int[] arr) {
+	String ret = "[";
+	for (int i : arr)
+	    ret += i + ", ";
+	return ret.substring(0, ret.length()-2) + "]";
+    }
+
+
+    public static void main(String[] args) {
+	//TESTING PAIRSUM()
+	System.out.println("\nTesting pairSum()...");
+	int[] arr = new int[]{11, 15, 6, 8, 9, 10, 11};
+	int sum = 16;
+	System.out.println("pairSum(" + printArray(arr) + ", " + 
+			   sum + ") -> ");
+	System.out.println( pairSum(arr, sum) );
+
+	sum = 27;
+	System.out.println("pairSum(" + printArray(arr) + ", " + 
+			   sum + ") -> ");
+	System.out.println( pairSum(arr, sum) );
+
+	//TESTING TRIPLETSUM()
+	System.out.println("\nTesting tripletSum()...");
+	sum = 25;
+	System.out.println("tripletSum(" + printArray(arr) + ", " + 
+			   sum + ") -> ");
+	System.out.println( tripletSum(arr, sum) );
+
+	sum = 39;
+	System.out.println("tripletSum(" + printArray(arr) + ", " + 
+			   sum + ") -> ");
+	System.out.println( tripletSum(arr, sum) );
+    }
+}
---
+++ hw1/CreditCard.java
@@ -0,0 +1,94 @@
+import java.util.Calendar;
+
+public class CreditCard {
+    private String creditCardNumber;
+    private String cardHolderName;
+    private String bank;
+    private int limit;
+    private double balance;
+
+    // Constructor
+    public CreditCard(String creditCardNumber, String cardHolderName,
+		      String bank, int limit, double balance) {
+	this.creditCardNumber = creditCardNumber;
+	this.cardHolderName = cardHolderName;
+	this.bank = bank;
+	this.limit = limit;
+	this.balance = balance;
+    }
+
+    //Accessor Methods
+    public String getCreditCardNumber() {return creditCardNumber;}
+    public String getCardHolderName() {return cardHolderName;}
+    public String getBank() {return bank;}
+    public int getLimit() {return limit;}
+    public double getBalance() {return balance;}
+    @Override
+	public String toString() {
+	return "CreditCard [creditCardNumber=" + creditCardNumber
+	    + ", cardHolderName='" + cardHolderName + "', bank = '" + bank
+	    + "', limit = " + limit + ", balance = " + balance + "]";
+	
+    }
+    
+
+
+    /***********************************
+     ASSIGNMENT STARTS HERE
+    ***********************************/
+
+    /* 
+     * chargeIt() takes price as an argument and returns whether
+     * transaction was successful or not
+     * 
+     * If post-balance doesn't exceed limit,
+     * charge account and return true.
+     * Returns false otherwise
+     *
+     * Precond: price is positive
+     */
+    public boolean chargeIt(double price) {
+	if (balance + price > limit) return false;
+	balance += price;
+	return true;
+    }
+    
+
+    /*
+     * Precond: amount is positive
+     */
+    public void payment(double amount) {
+	//check for late fee (15th of month)
+	Calendar c = Calendar.getInstance();
+	if (c.get(Calendar.DAY_OF_MONTH) > 15) {
+	    //change late fee here
+	    double late_fee = 0.05 * balance;
+	    balance += late_fee;
+	}
+	balance -= amount;
+    }
+
+
+    public static void main(String[] args) {
+	CreditCard cc = new CreditCard("5498300036895870", 
+				       "John Doe", "Visa", 
+				       1000, 0.0);
+	System.out.println("Testing chargeIt...");
+	System.out.println(cc);
+
+	System.out.println("\ncc.chargeIt(700.00) -> " + cc.chargeIt(700.0));
+	System.out.println("balance = " + cc.getBalance());
+	
+	System.out.println("\ncc.chargeIt(700.00) -> " + cc.chargeIt(700.0));
+	System.out.println("balance = " + cc.getBalance());
+
+	System.out.println("\ncc.chargeIt(300.00) -> " + cc.chargeIt(300.0));
+	System.out.println("balance = " + cc.getBalance());
+
+	System.out.println("\nRunning cc.payment(243.92)...");
+	cc.payment(243.92);
+	System.out.println("balance = " + cc.getBalance());
+
+	//wait 15 days time for testing late/not-late payment functionality
+    }
+}
